da3b5a92ab12d98cdc9d64a2cd314143f6de8de5
Gabriel Tavares <gabriel-tavares2009@hotmail.com>
2023-08-15 21:04:58 -0300

    Added new meta tags

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,8 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
     title: 'DownTube - Next.js',
     description: 'Download YouTube videos and musics with this Next.js app.',
+    image: 'https://i.imgur.com/2Q7GUig.png',
+    keywords: 'downtube,youtube downloader,nextjs,react,procet,download youtube,youtube mp3'
 }
 
 export default function RootLayout({ children }) {
@@ -15,7 +17,7 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel='icon' href='/favicon.ico' />
             </head>
-            <body className={inter.className} style={{ backgroundColor: '#323232', overflowY: 'hidden' }}>{children}</body>
+            <body className={inter.className} style={{ backgroundColor: '#323232' }}>{children}</body>
         </html>
     )
 }
