c33d21baaa7e05579b0f998320ad4daead24b70a
Gabriel Tavares <gabriel-tavares2009@hotmail.com>
2023-08-21 11:31:51 -0300

    Fixed conver bytes to string error in download api

--- app/api/download/route.js
+++ app/api/download/route.js
@@ -40,15 +40,6 @@ export const POST = async req => {
     }
 
     var responseHeaders = new Headers();
-    responseHeaders.set(
-        "Content-Disposition",
-        `attachment; filename="${videoName}.${itagExists[0].mimeType.includes('audio') ? 'm4a' : 'mp4'}"`,
-    );
-
-    responseHeaders.set(
-        "User-Agent",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-    );
 
     let contentLength = 0;
     const data = ytdl(videoId, {
