81bedd7820dc61a98b3dd65778c8f7a96cda560e
Gabriel Tavares <hello@gabrieltavares.dev>
2024-07-12 16:23:17 -0300

    fixing ytdl-core bug

--- app/api/download/route.js
+++ app/api/download/route.js
@@ -1,42 +1,51 @@
 import { NextResponse } from "next/server";
-const ytdl = require('ytdl-core');
+const ytdl = require("@distube/ytdl-core");
 
-export const POST = async req => {
-    const jsonBody = await req.json()
-    const { videoId, itag } = jsonBody
+export const POST = async (req) => {
+    const jsonBody = await req.json();
+    const { videoId, itag } = jsonBody;
 
     if (!videoId || !itag) {
-        return NextResponse.json({
-            success: false,
-            error: 'Provide videoId and itag parameters.'
-        }, {
-            status: 400
-        })
+        return NextResponse.json(
+            {
+                success: false,
+                error: "Provide videoId and itag parameters.",
+            },
+            {
+                status: 400,
+            },
+        );
     }
 
     if (!ytdl.validateID(videoId)) {
-        return NextResponse.json({
-            success: false,
-            error: 'Invalid videoId parameter.'
-        }, {
-            status: 400
-        })
+        return NextResponse.json(
+            {
+                success: false,
+                error: "Invalid videoId parameter.",
+            },
+            {
+                status: 400,
+            },
+        );
     }
 
-    
-    const {videoName, itagExists} = await ytdl.getBasicInfo(`https://www.youtube.com/watch?v=${videoId}`)
-        .then(info => ({
+    const { videoName, itagExists } = await ytdl
+        .getBasicInfo(`https://www.youtube.com/watch?v=${videoId}`)
+        .then((info) => ({
             videoName: info.player_response.videoDetails.title,
-            itagExists: info.formats.filter(item => item.itag == itag)
-        }))
-    
-    if (!itagExists){
-        return NextResponse.json({
-            success: false,
-            error: 'Invalid quality/format selected.'
-        }, {
-            status: 400
-        })
+            itagExists: info.formats.filter((item) => item.itag == itag),
+        }));
+
+    if (!itagExists) {
+        return NextResponse.json(
+            {
+                success: false,
+                error: "Invalid quality/format selected.",
+            },
+            {
+                status: 400,
+            },
+        );
     }
 
     var responseHeaders = new Headers();
@@ -44,15 +53,13 @@ export const POST = async req => {
     let contentLength = 0;
     const data = ytdl(videoId, {
         quality: itag,
-        filter: format => format.audioTrack ? format.audioTrack.audioIsDefault : format
-    })
-    
-    responseHeaders.set(
-        "Total-Bytes",
-        `${contentLength}`
-    )
+        filter: (format) =>
+            format.audioTrack ? format.audioTrack.audioIsDefault : format,
+    });
+
+    responseHeaders.set("Total-Bytes", `${contentLength}`);
 
     return new Response(data, {
         headers: responseHeaders,
     });
-}
+};
--- app/api/getInfo/route.js
+++ app/api/getInfo/route.js
@@ -1,27 +1,33 @@
 import { NextResponse } from "next/server";
-const ytdl = require('ytdl-core');
-const url = require('url');
+const ytdl = require("@distube/ytdl-core");
+const url = require("url");
 
-export const POST = async req => {
-    const jsonBody = await req.json()
-    const videoId = jsonBody.videoId
+export const POST = async (req) => {
+    const jsonBody = await req.json();
+    const videoId = jsonBody.videoId;
 
     if (!videoId) {
-        return NextResponse.json({
-            success: false,
-            error: 'Provide videoId parameter.'
-        }, {
-            status: 400
-        })
+        return NextResponse.json(
+            {
+                success: false,
+                error: "Provide videoId parameter.",
+            },
+            {
+                status: 400,
+            },
+        );
     }
 
     if (!ytdl.validateID(videoId)) {
-        return NextResponse.json({
-            success: false,
-            error: 'Invalid videoId parameter.'
-        }, {
-            status: 400
-        })
+        return NextResponse.json(
+            {
+                success: false,
+                error: "Invalid videoId parameter.",
+            },
+            {
+                status: 400,
+            },
+        );
     }
 
     const qualityOrder = {
@@ -37,50 +43,67 @@ export const POST = async req => {
 
     const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
 
-    const { data, status_code } = await ytdl.getInfo(videoUrl, {})
-        .catch(err => console.log(`Err ${err}`))
-        .then(async info => {
-            const formatData = await Promise.all(info.formats.sort((a, b) => {
-                const qualityA = qualityOrder[a.qualityLabel] !== undefined ? qualityOrder[a.qualityLabel] : 8; // Assign a higher order for audio
-                const qualityB = qualityOrder[b.qualityLabel] !== undefined ? qualityOrder[b.qualityLabel] : 8; // Assign a higher order for audio
+    const { data, status_code } = await ytdl
+        .getInfo(videoUrl, {})
+        .catch((err) => console.log(`Err ${err}`))
+        .then(async (info) => {
+            const formatData = await Promise.all(
+                info.formats
+                    .sort((a, b) => {
+                        const qualityA =
+                            qualityOrder[a.qualityLabel] !== undefined
+                                ? qualityOrder[a.qualityLabel]
+                                : 8; // Assign a higher order for audio
+                        const qualityB =
+                            qualityOrder[b.qualityLabel] !== undefined
+                                ? qualityOrder[b.qualityLabel]
+                                : 8; // Assign a higher order for audio
 
-                if (qualityA !== qualityB) {
-                    return qualityA - qualityB;
-                }
+                        if (qualityA !== qualityB) {
+                            return qualityA - qualityB;
+                        }
 
-                return a.mimeType.localeCompare(b.mimeType);
-            })
-                .filter(value => (value.qualityLabel && value.audioQuality) || (value.audioQuality && !value.qualityLabel && value.mimeType.includes('mp4')))
-                .map(async value => {
-                    if (!value.contentLength) {
-                        var parsed = url.parse(value.url);
-                        parsed.method = 'HEAD';
-                        const res = await fetch(value.url);
-                        return {
-                            ...value,
-                            contentLength: res.headers.get('content-length')
-                        };
-                    }
-                    return value;
-            }))
+                        return a.mimeType.localeCompare(b.mimeType);
+                    })
+                    .filter(
+                        (value) =>
+                            (value.qualityLabel && value.audioQuality) ||
+                            (value.audioQuality &&
+                                !value.qualityLabel &&
+                                value.mimeType.includes("mp4")),
+                    )
+                    .map(async (value) => {
+                        if (!value.contentLength) {
+                            var parsed = url.parse(value.url);
+                            parsed.method = "HEAD";
+                            const res = await fetch(value.url);
+                            return {
+                                ...value,
+                                contentLength:
+                                    res.headers.get("content-length"),
+                            };
+                        }
+                        return value;
+                    }),
+            );
 
             if (info.player_response.videoDetails.isPrivate) {
                 return {
                     data: {
                         success: false,
-                        error: "Can't download private content."
+                        error: "Can't download private content.",
                     },
-                    status_code: 400
-                }
+                    status_code: 400,
+                };
             }
             if (info.player_response.videoDetails.isLiveContent) {
                 return {
                     data: {
                         success: false,
-                        error: "Can't download live content."
+                        error: "Can't download live content.",
                     },
-                    status_code: 400
-                }
+                    status_code: 400,
+                };
             }
 
             return {
@@ -88,14 +111,15 @@ export const POST = async req => {
                     success: true,
                     details: {
                         ...info.player_response.videoDetails,
-                        ...info.videoDetails
+                        ...info.videoDetails,
                     },
-                    formats: formatData
-                }, status_code: 200
-            }
-        })
+                    formats: formatData,
+                },
+                status_code: 200,
+            };
+        });
 
     return NextResponse.json(data, {
-        status: status_code
-    })
-}
+        status: status_code,
+    });
+};
